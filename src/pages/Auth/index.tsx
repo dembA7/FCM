327192970580eff815ac749c09797ac37eb80d26
dembA7 <a01709522@tec.mx>
2024-04-18 00:26:02 -0600

    update files

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -6,11 +6,22 @@ import googleImage from '../../assets/images/google-logo.webp';
 import loginImage from '../../assets/images/login_image.png';
 import { auth, provider } from '../../config/firebase.config';
 import { RoutesPath } from '../../utils/constants';
-import { requestPermission } from '../../config/firebase-messaging-sw';
+import { requestPermission } from '../../../firebase-messaging-sw';
 
 const Auth: React.FC = () => {
 
   requestPermission();
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/firebase-messaging-sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
 
   const navigate = useNavigate();
 
