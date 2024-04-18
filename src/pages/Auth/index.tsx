a546c6ae3694c7218a97dc531e992e6cfb874c23
dembA7 <a01709522@tec.mx>
2024-04-18 00:45:51 -0600

    update files

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -12,6 +12,17 @@ const Auth: React.FC = () => {
 
   requestPermission();
 
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/firebase-messaging-sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
+
   const navigate = useNavigate();
 
   const handleGoogleSignIn = async () => {
